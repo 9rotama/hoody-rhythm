22c17d3e7add2aced9d79ff9aabe9cdb28e17382
9rotama <65887771+9rotama@users.noreply.github.com>
2023-08-15 01:41:39 +0900

    ノーツの色を減らしてかんたんにした

--- src/const.ts
+++ src/const.ts
@@ -1,21 +1,11 @@
 export const noteTypeKeyMaps = [
   {
-    key: "z",
+    key: "d",
     name: "red",
     noteColor: "#FF0060",
   },
   {
-    key: "x",
-    name: "yellow",
-    noteColor: "#F6FA70",
-  },
-  {
-    key: "c",
-    name: "green",
-    noteColor: "#00DFA2",
-  },
-  {
-    key: "v",
+    key: "j",
     name: "blue",
     noteColor: "#0079FF",
   },
