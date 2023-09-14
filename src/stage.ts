450dd4091ab2ee2399994fc55ef593f84c559c9d
9rotama <65887771+9rotama@users.noreply.github.com>
2023-09-14 20:48:38 +0900

    フォルダのモデル追加

--- src/stage.ts
+++ src/stage.ts
@@ -1,5 +1,7 @@
-import { scene } from "./main";
+import { rootClock, scene } from "./main";
 import * as THREE from "three";
+import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
+import { update } from "three/examples/jsm/libs/tween.module.js";
 
 const floorGeometry = new THREE.PlaneGeometry(100, 100, 32, 32);
 const glassFloorMaterial = new THREE.MeshPhongMaterial({
@@ -23,8 +25,56 @@ gridFloor.rotation.x = 90;
 
 const fog = new THREE.Fog("#ddddff", 1, 40);
 
+const loader = new GLTFLoader();
+
+let folderGltfModel: THREE.Group | undefined = undefined;
+
+const FolderMaterial = new THREE.MeshPhongMaterial({
+  color: "#FFDE9E",
+  side: THREE.DoubleSide,
+});
+const folderPos = new THREE.Vector3(-10, 0, -0.2);
+
 export const initStage = () => {
   scene.add(glassFloor);
   scene.add(gridFloor);
   scene.fog = fog;
+
+  loader.load(
+    "folder.gltf",
+    (gltf) => {
+      folderGltfModel = gltf.scene;
+      folderGltfModel.scale.set(7, 7, 7);
+      folderGltfModel.position.set(folderPos.x, folderPos.y, folderPos.z);
+      folderGltfModel.traverse((child) => {
+        const childMesh = child as THREE.Mesh;
+        if (childMesh.isMesh) {
+          childMesh.material = FolderMaterial;
+          childMesh.castShadow = true;
+        }
+      });
+      scene.add(folderGltfModel);
+    },
+    undefined,
+    (error) => {
+      console.error(error);
+    }
+  );
+
+  updateStage();
+};
+
+export const updateStage = () => {
+  requestAnimationFrame(updateStage);
+  moveFolder();
+};
+
+export const moveFolder = () => {
+  if (!folderGltfModel) return;
+  const t = rootClock.getElapsedTime();
+  folderGltfModel.rotation.set(
+    0,
+    Math.sin(t) * 0.2 + 5,
+    Math.sin(t * 0.3) * 0.2
+  );
 };
